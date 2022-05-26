365458e00ed76d3ff4a2e1f0655c2efbdd5a8f13
Sander Vanheule <sander@svanheule.net>
2022-05-26 09:11:29 +0200

    tplink-safeloader: join EAP225-V3 compatible devices
    Since FW v5.0.0, TP-Link has provided unified firmware images for
    EAP225-v3 and EAP225-Outdoor-v1 devices. Although there may be hardware
    differences, the relevant details for image generation are identical.
    
    More recently, the following devices have also been added to the support
    list:
      - EAP225-v3.20
      - EAP225-Outdoor-v3
      - EAP225-v4
    
    The country codes appended the device descriptors were introduced with
    FW v5.0.7: "Combined firmware versions for different countries/regions."
    The release notes provide no further upgrade instructions, so these
    firmware images should be backwards compatible.
    
    Add the new compatible devices, with region codes, to the support list
    of the EAP225-V3 board ID. The now redundant EAP225-OUTDOOR-V1 ID will
    be dropped later.
    
    Signed-off-by: Sander Vanheule <sander@svanheule.net>

--- src/tplink-safeloader.c
+++ src/tplink-safeloader.c
@@ -1662,12 +1662,22 @@ static struct device_info boards[] = {
 		.last_sysupgrade_partition = "file-system"
 	},
 
-	/** Firmware layout for the EAP225 v3 */
+	/** Firmware layout for the EAP225 v3
+	 * Also compatible with:
+	 *   - EAP225 v3.20
+	 *   - EAP225 v4
+	 *   - EAP225-Outdoor v1
+	 *   - EAP225-Outdoor v3
+	 *   */
 	{
 		.id     = "EAP225-V3",
 		.support_list =
 			"SupportList:\r\n"
-			"EAP225(TP-Link|UN|AC1350-D):3.0\r\n",
+			"EAP225(TP-Link|UN|AC1350-D):3.0\r\n"
+			"EAP225(TP-Link|UN|AC1350-D):3.20\r\n"
+			"EAP225(TP-Link|UN|AC1350-D):4.0 CA\r\n"
+			"EAP225-Outdoor(TP-Link|UN|AC1350-D):1.0\r\n"
+			"EAP225-Outdoor(TP-Link|UN|AC1350-D):3.0 CA,JP\r\n",
 		.part_trail = PART_TRAIL_NONE,
 		.soft_ver = SOFT_VER_DEFAULT,
 		.soft_ver_compat_level = 1,
