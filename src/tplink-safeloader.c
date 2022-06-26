openwrt/firmware-utils

commit 9563fe8e78cb45cbfcc3c6bf5dc782b4829b1182

tplink-safeloader: add regionless EAP225-V3 IDs
Devices whose model ID does not contain a region identifier will not
match the "CA" and "CA,JP" qualified model IDs in the current EAP225-V3
SupportList. This results in the stock firmware giving the following
error when trying to flash a factory image:
    Upgrade failed because the firmware is incompatible

Add the IDs without region qualifiers to also support these devices.

Signed-off-by: Sander Vanheule <sander@svanheule.net>

src/tplink-safeloader.c
@@ -1675,8 +1675,10 @@ static struct device_info boards[] = {
 			"SupportList:\r\n"
 			"EAP225(TP-Link|UN|AC1350-D):3.0\r\n"
 			"EAP225(TP-Link|UN|AC1350-D):3.20\r\n"
+			"EAP225(TP-Link|UN|AC1350-D):4.0\r\n"
 			"EAP225(TP-Link|UN|AC1350-D):4.0 CA\r\n"
 			"EAP225-Outdoor(TP-Link|UN|AC1350-D):1.0\r\n"
+			"EAP225-Outdoor(TP-Link|UN|AC1350-D):3.0\r\n"
 			"EAP225-Outdoor(TP-Link|UN|AC1350-D):3.0 CA,JP\r\n",
 		.part_trail = PART_TRAIL_NONE,
 		.soft_ver = SOFT_VER_DEFAULT,

commit 1e3d47292b2efbe2665072fa8322f44bd063be39

tplink-safeloader: fix EAP225-Outdoor model IDs
The model IDs for EAP225-Outdoor v1 and EAP225-Outdoor v3 contain
"AC1200". The corresponding identifiers grouped under EAP225-V3 contain
"AC1350" however, which is a copy-paste error from the EAP225 v3/v3.2/v4
IDs. Modify the strings in SupportList to actually match the ones on devices.

Fixes: 9563fe8e78cb ("tplink-safeloader: add regionless EAP225-V3 IDs")
Fixes: 365458e00ed7 ("tplink-safeloader: join EAP225-V3 compatible devices")
Signed-off-by: Sander Vanheule <sander@svanheule.net>

src/tplink-safeloader.c
@@ -1677,9 +1677,9 @@ static struct device_info boards[] = {
 			"EAP225(TP-Link|UN|AC1350-D):3.20\r\n"
 			"EAP225(TP-Link|UN|AC1350-D):4.0\r\n"
 			"EAP225(TP-Link|UN|AC1350-D):4.0 CA\r\n"
-			"EAP225-Outdoor(TP-Link|UN|AC1350-D):1.0\r\n"
-			"EAP225-Outdoor(TP-Link|UN|AC1350-D):3.0\r\n"
-			"EAP225-Outdoor(TP-Link|UN|AC1350-D):3.0 CA,JP\r\n",
+			"EAP225-Outdoor(TP-Link|UN|AC1200-D):1.0\r\n"
+			"EAP225-Outdoor(TP-Link|UN|AC1200-D):3.0\r\n"
+			"EAP225-Outdoor(TP-Link|UN|AC1200-D):3.0 CA,JP\r\n",
 		.part_trail = PART_TRAIL_NONE,
 		.soft_ver = SOFT_VER_DEFAULT,
 		.soft_ver_compat_level = 1,