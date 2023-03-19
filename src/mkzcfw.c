openwrt/firmware-utils

commit 16cff6c7ca36ed6d14aea66461681c31cc698087

firmware-utils: fix unused variable warnings
This fixes warnings from the:
1. -Wunused-variable
2. -Wunused-but-set-variable

Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

src/mkzcfw.c
@@ -127,7 +127,6 @@ static struct board_info *find_board(char *id)
 static void usage(int status)
 {
 	FILE *stream = (status != EXIT_SUCCESS) ? stderr : stdout;
-	struct board_info *board;
 
 	fprintf(stream, "Usage: %s [OPTIONS...]\n", progname);
 	fprintf(stream,
@@ -276,8 +275,6 @@ static int build_fw(void)
 	char *buf;
 	char *p;
 	int ret = EXIT_FAILURE;
-	int writelen = 0;
-	uint32_t crc;
 	struct fw_header *hdr;
 	struct fw_tail *tail;
 
@@ -361,11 +358,6 @@ static int build_fw(void)
 int main(int argc, char *argv[])
 {
 	int ret = EXIT_FAILURE;
-	int err;
-
-	FILE *outfile;
-
-	progname = basename(argv[0]);
 
 	while ( 1 ) {
 		int c;

src/bcmalgo.c
@@ -98,7 +98,6 @@ uint16_t get_hcs ( ldr_header_t* hd )
 	uint8_t hcs_minor;
 	uint8_t hcs_major;
 	uint16_t n = 0xffff;
-	uint16_t m = 0;
 	int state = 0;
 	int i,j;
 	for ( i = 0; i < 0x54; i++ )
@@ -226,7 +225,7 @@ int dump_header ( ldr_header_t* hd )
 	printf ( "HNW:\t\t0x%04X\n",reverse_endian16 ( hd->her_znaet_chto ) ); //Hell knows what
 	printf ( "CRC:\t\t0x%08X\n",reverse_endian32 ( hd->crc ) );
 	printf ( "=== Binary Header Dump===\n" );
-	int i,j;
+	int i;
 	uint8_t* head = ( uint8_t* ) hd;
 	for ( i=0;i<=sizeof ( ldr_header_t );i++ )
 	{

src/dns313-header.c
@@ -126,16 +126,13 @@ int main(int argc, char **argv)
 	int fdout;
 	struct stat sb;
 	uint32_t filesize;
-	uint32_t padding;
 	int ret = 0;
 	const char *pathin;
 	const char *pathout;
 	unsigned char *buffer;
-	unsigned char *infop;
 	uint32_t sum;
 	size_t bufsize;
 	size_t bytes;
-	int i;
 
 	if (argc < 3) {
 		printf("Too few arguments.\n");
@@ -150,7 +147,6 @@ int main(int argc, char **argv)
 		return ret;
 
 	filesize = sb.st_size;
-	padding = filesize % 4;
 	printf("INFILE: %s, size: %08x bytes\n", pathin, filesize);
 	/* File + extended header size */
 	bufsize = filesize + HEADER_SIZE;
@@ -179,9 +175,6 @@ int main(int argc, char **argv)
 		return 0;
 	}
 	close(fdin);
-
-	/* PREP HEADER AND FOOTER */
-	infop = buffer;
 
 	be_wr(buffer + OFFSET_MAGIC, IH_MAGIC);
 

src/imagetag.c
@@ -82,8 +82,8 @@ int tagfile(const char *kernel, const char *rootfs, const char *bin, \
 	struct bcm_tag tag;
 	struct kernelhdr khdr;
 	FILE *kernelfile = NULL, *rootfsfile = NULL, *binfile = NULL, *cfefile = NULL;
-	size_t cfeoff, cfelen, kerneloff, kernellen, rootfsoff, rootfslen, \
-	  read, imagelen, rootfsoffpadlen = 0, kernelfslen, kerneloffpadlen = 0, oldrootfslen, \
+	size_t cfelen, kerneloff, kernellen, rootfsoff, rootfslen, \
+	  read, imagelen, rootfsoffpadlen = 0, oldrootfslen, \
 	  rootfsend;
 	uint8_t readbuf[1024];
 	uint32_t imagecrc = IMAGETAG_CRC_START;
@@ -91,7 +91,6 @@ int tagfile(const char *kernel, const char *rootfs, const char *bin, \
 	uint32_t rootfscrc = IMAGETAG_CRC_START;
 	uint32_t kernelfscrc = IMAGETAG_CRC_START;
 	uint32_t fwaddr = 0;
-	uint8_t crc_val;
 	const uint32_t deadcode = htonl(DEADCODE);
 	int i;
 	int is_pirelli = 0;
@@ -126,7 +125,6 @@ int tagfile(const char *kernel, const char *rootfs, const char *bin, \
 
 	fwaddr = flash_start + image_offset;
 	if (cfefile) {
-	  cfeoff = flash_start;		  
 	  cfelen = getlen(cfefile);
 	  /* Seek to the start of the file after tag */
 	  fseek(binfile, sizeof(tag), SEEK_SET);
@@ -138,7 +136,6 @@ int tagfile(const char *kernel, const char *rootfs, const char *bin, \
 	  }
 
 	} else {
-	  cfeoff = 0;
 	  cfelen = 0;
 	}
 
@@ -239,7 +236,6 @@ int tagfile(const char *kernel, const char *rootfs, const char *bin, \
 	  oldrootfslen = getlen(rootfsfile);
 	  rootfslen = oldrootfslen;
 	  rootfslen = ( (rootfslen % block_size) > 0 ? (((rootfslen / block_size) + 1) * block_size) : rootfslen );
-	  kerneloffpadlen = rootfslen - oldrootfslen;
 	  oldrootfslen = rootfslen;
 
 	  kerneloff = rootfsoff + rootfslen;
@@ -391,7 +387,6 @@ int tagfile(const char *kernel, const char *rootfs, const char *bin, \
 
 int main(int argc, char **argv)
 {
-    int c, i;
 	char *kernel, *rootfs, *bin;
 	uint32_t flash_start, image_offset, block_size, load_address, entry;
 	flash_start = image_offset = block_size = load_address = entry = 0;

src/mkcasfw.c
@@ -763,8 +763,7 @@ write_out_header(FILE *outfile)
 int
 write_out_images(FILE *outfile)
 {
-	struct image_desc *desc;
-	int i, res;
+	int res;
 
 	res = image_writeout(outfile, &kernel_image);
 	if (res)
@@ -839,7 +838,6 @@ parse_opt_image(char ch, char *arg)
 {
 	char buf[MAX_ARG_LEN];
 	char *argv[MAX_ARG_COUNT];
-	int argc;
 	char *p;
 	struct image_desc *desc = NULL;
 	int i;
@@ -864,7 +862,7 @@ parse_opt_image(char ch, char *arg)
 	if (!desc)
 		return ERR_FATAL;
 
-	argc = parse_arg(arg, buf, argv);
+	parse_arg(arg, buf, argv);
 
 	i = 0;
 	p = argv[i++];
@@ -886,9 +884,6 @@ parse_opt_image(char ch, char *arg)
 int
 process_images(void)
 {
-	struct image_desc *desc;
-	uint32_t offs = 0;
-	int i;
 	int res;
 
 	kernel_image.out_size = board->max_kernel_size;

src/mkcsysimg.c
@@ -792,7 +792,6 @@ parse_opt_block(char ch, char *arg)
 {
 	char buf[MAX_ARG_LEN];
 	char *argv[MAX_ARG_COUNT];
-	int argc;
 	char *p;
 	struct csys_block *block;
 	int i;
@@ -854,7 +853,7 @@ parse_opt_block(char ch, char *arg)
 		return ERR_FATAL;
 	}
 
-	argc = parse_arg(arg, buf, argv);
+	parse_arg(arg, buf, argv);
 
 	i = 0;
 	p = argv[i++];

src/mkdniimg.c
@@ -50,7 +50,6 @@ static char *board_id;
 void usage(int status)
 {
 	FILE *stream = (status != EXIT_SUCCESS) ? stderr : stdout;
-	struct board_info *board;
 
 	fprintf(stream, "Usage: %s [OPTIONS...]\n", progname);
 	fprintf(stream,

src/mkfwimage2.c
@@ -65,7 +65,6 @@ typedef struct image_info {
 } image_info_t;
 
 static image_info_t im;
-static int debug = 0;
 static int zero_part_baseaddr = 0;
 
 static void write_header(void* mem, const char* version)

commit d9b6460c168e2116f7c1808f547608bb83c9ac89

firmware-utils: fix use of NULL string progname
The compiler complains about several lines which use `progname`,
but that char pointer was NULL.

Fixes: 16cff6c7ca36 ("firmware-utils: fix unused variable warnings")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/mkzcfw.c
@@ -356,6 +356,8 @@ int main(int argc, char *argv[])
 {
 	int ret = EXIT_FAILURE;
 
+	progname = basename(argv[0]);
+
 	while ( 1 ) {
 		int c;
 