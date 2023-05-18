de36fa6af20b2c3890c263c938a472a58005cbc1
Aleksandar Tanev <33719472+atanev@users.noreply.github.com>
2023-05-18 09:58:10 +0200

    tplink-safeloader: add AX23 support-list entry
    Signed-off-by: Aleksandar Tanev <33719472+atanev@users.noreply.github.com>
    Signed-off-by: David Bauer <mail@david-bauer.net>

--- src/tplink-safeloader.c
+++ src/tplink-safeloader.c
@@ -987,6 +987,7 @@ static struct device_info boards[] = {
 			"{product_name:Archer AX23,product_ver:1.20,special_id:45550000}\n"
 			"{product_name:Archer AX23,product_ver:1.20,special_id:4A500000}\n"
 			"{product_name:Archer AX23,product_ver:1.20,special_id:55530000}\n"
+			"{product_name:Archer AX1800,product_ver:1.20,special_id:45550000}\n"
 			"{product_name:Archer AX1800,product_ver:1.20,special_id:52550000}\n",
 		.part_trail = 0x00,
 		.soft_ver = SOFT_VER_TEXT("soft_ver:3.0.3\n"),
