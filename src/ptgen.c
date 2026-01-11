openwrt/firmware-utils

commit 0782d243d23e64d8420f8c067a4169eb69a55f92

Revert "ptgen: do not create stub partition to fill a gap if gap caused by alignment"
Without the BIOS Boot Partition in the gap GRUB fails to install on
MBR-boot images.

Hence revert commit f1f98b0b8456e89f6c959d8f028c5204da82365e.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

src/ptgen.c
@@ -489,8 +489,7 @@ static int gen_gptable(uint32_t signature, guid_t guid, unsigned nr)
 		printf("%" PRIu64 "\n", (sect - start) * DISK_SECTOR_SIZE);
 	}
 
-	if ((parts[0].start != 0) &&
-	    (parts[0].actual_start > gpt_first_entry_sector + GPT_SIZE)) {
+	if (parts[0].actual_start > GPT_FIRST_ENTRY_SECTOR + GPT_SIZE) {
 		gpte[GPT_ENTRY_MAX - 1].start = cpu_to_le64(gpt_first_entry_sector + GPT_SIZE);
 		gpte[GPT_ENTRY_MAX - 1].end = cpu_to_le64(parts[0].actual_start - 1);
 		gpte[GPT_ENTRY_MAX - 1].type = GUID_PARTITION_BIOS_BOOT;

commit f890bd41225f86a29b0c851f4659a6f9ee68a25b

ptgen: fix bug caused by not completely correct reverts
The commit 0782d243d23e (Revert "ptgen: do not create stub partition to
fill a gap if gap caused by alignment") fixes one issue but introduce
another.

If "-e <gpt_entry_offs>" option is used and there is no gap between GPT
Entry Table and 1-st partition, then

 1) A GPT stub partition will be created, but this should not be done
    because of no space for it.
 2) A stub partition will be incorrect (start_sector > end_sector)

This patch fixes an issue.

Fixes: 0782d243d23e (Revert "ptgen: do not create stub partition to fill a gap if gap caused by alignment")
Signed-off-by: Mikhail Kshevetskiy <mikhail.kshevetskiy@iopsys.eu>

src/ptgen.c
@@ -489,7 +489,7 @@ static int gen_gptable(uint32_t signature, guid_t guid, unsigned nr)
 		printf("%" PRIu64 "\n", (sect - start) * DISK_SECTOR_SIZE);
 	}
 
-	if (parts[0].actual_start > GPT_FIRST_ENTRY_SECTOR + GPT_SIZE) {
+	if (parts[0].actual_start > gpt_first_entry_sector + GPT_SIZE) {
 		gpte[GPT_ENTRY_MAX - 1].start = cpu_to_le64(gpt_first_entry_sector + GPT_SIZE);
 		gpte[GPT_ENTRY_MAX - 1].end = cpu_to_le64(parts[0].actual_start - 1);
 		gpte[GPT_ENTRY_MAX - 1].type = GUID_PARTITION_BIOS_BOOT;